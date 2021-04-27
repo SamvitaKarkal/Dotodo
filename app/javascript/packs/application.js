c6f9fc7c850a7185f416a23af584480a14d5730a
Samvita_Karkal <samvitaskarkal@gmail.com>
2021-04-27 20:18:39 +0530

    Displaying task list on dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,5 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 // Support component names relative to this directory:
-var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 // import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Dashboard from "components/Dashboard";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+// const App = () => {
+//   // useEffect(() => {
+//   //   /*eslint no-undef: "off"*/
+//   //   initializeLogger();
+//   //   logger.info("Log from js-logger");
+//   // }, []);
+  
+//   return (
+//     // <Router>
+//     //   <Switch>
+//     //     <Route exact path="/" render={() => <div>Home</div>} />
+//     //     <Route exact path="/about" render={() => <div>About</div>} />
+//     //   </Switch>
+//     // </Router>
+
+//     <h1>This is App.jsx</h1>
+//   );
+// };
 
 const App = () => {
-  // useEffect(() => {
-  //   /*eslint no-undef: "off"*/
-  //   initializeLogger();
-  //   logger.info("Log from js-logger");
-  // }, []);
-  
   return (
-    // <Router>
-    //   <Switch>
-    //     <Route exact path="/" render={() => <div>Home</div>} />
-    //     <Route exact path="/about" render={() => <div>About</div>} />
-    //   </Switch>
-    // </Router>
-
-    <h1>This is App.jsx</h1>
+    <Router>
+      <Switch>
+        {/* // <--- rest of the code if any -----> */}
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
   );
 };
-
 export default App;
